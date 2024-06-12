7c2a4f553d59acbbb51ee1c3a33b7d70548e8d76
Swpee <s.suwapee@gmail.com>
2024-06-12 15:08:57 +0700

    add: Assignment : JavaScript Loop Assignment

--- ex-2.js
+++ ex-2.js
@@ -2,5 +2,10 @@
 let numbers = [10, 20, 30, 40, 50];
 let total = 0;
 // Start coding here
+let index =0;
+while (index<numbers.length) {
+    total = total+numbers[index]
+    index++;
+}
 
 console.log(total);
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,16 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
 let minScore;
+
 // Start coding here
+
+for (let index = 0; index < scores.length; index++) {
+    if (minScore<scores[index]) {
+        minScore = minScore
+    } else {
+        minScore = scores[index]
+    }        
+}
+
 
 console.log(minScore);
